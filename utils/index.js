fc3ff3e76178d7831851a73fa0cc8e1a478c0522
vick12052002 <vick12052002@gmail.com>
2025-06-16 15:36:12 +0800

    feat: add debounce fn

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+export function debounce(func, delay) {
+  let timeout;
+  
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
